12a31b22e1a6980faed490fbeb277cf54695d48c
Evangeline0401 <32709664+Evangeline0401@users.noreply.github.com>
2020-06-23 21:18:41 +0900

    Convert 3

--- cfr_vanilla/sample.py
+++ cfr_vanilla/sample.py
@@ -6,28 +6,42 @@ action = [[0, 1, 2, 3, 4],
 
 
 def CFR(history, i, t, pi_i, pi_other, I_map):
-
     if check_terminal(history):
         return 0 #return utility
     elif check_chance(history):
         return 1
     
-    info_set = get_info_set(I_map, history)
+    info_set, info_set_player = get_info_set(I_map, history)
 
+    info_set.next_strategy(info_set_player)
     strategy = info_set.strategy
+
+    action_utils = np.zeros(len(action[info_set_player]))
+    for ite, act in enumerate(action[info_set_player]):
+        next_history = history + str(act)
+        if info_set_player == i:
+            action_utils[ite] = CFR(next_history, i, t,
+                                    strategy[ite]*pi_i, pi_other, I_map)
+        else:
+            action_utils[ite] = CFR(next_history, i, t,
+                                    pi_i, strategy[ite]*pi_other, I_map)
+    util = sum(action_utils * strategy)
+
+    if info_set_player == i:
+        for ite, act in enumerate(action[info_set_player]):
+            info_set.regret_sum[ite] += pi_other * ( action_utils[ite] - util )
+    
+    return util
 
 
 def check_terminal(history):
-
     return len(history) == 3
 
 
 def check_chance(history):
-
     return len(history) == 2
 
 def get_info_set(I_map, history):
-
     key = len(history)
     if key == 0:
         info_set_player = 0
@@ -38,9 +52,9 @@ def get_info_set(I_map, history):
     if key not in I_map:
         info_set = InformationSet(key, info_set_player)
         I_map[key] = info_set
-        return info_set
+        return info_set, info_set_player
 
-    return I_map[key]
+    return I_map[key], info_set_player
 
 
 class InformationSet():
@@ -49,13 +63,33 @@ class InformationSet():
         self.regret_sum = np.zeros(len(action[info_set_player]))
         self.strategy_sum = np.zeros(len(action[info_set_player]))
         self.strategy = np.repeat(1/len(action[info_set_player]), len(action[info_set_player]))
-        self.reach_pr = 0
+        self.reach_pr = 0#最終的な出力を表示するのに使う？
         self.reach_pr_sum = 0
+    
+    def next_strategy(self, info_set_player):
+        self.strategy_sum += self.reach_pr * self.strategy
+        self.strategy = self.calc_strategy(info_set_player)
+        self.reach_pr_sum += self.reach_pr
+        self.reach_pr = 0
+    
+    def calc_strategy(self, info_set_player):
+        strategy = self.make_positive(self.regret_sum)
+        total = sum(strategy)
+        if total > 0:
+            strategy = strategy / total
+        else:
+            n = len(action[info_set_player])
+            strategy = np.repeat(1/n, n)
+
+        return strategy
+    
+    def make_positive(self, x):
+        return np.where(x > 0, x, 0)
 
 
 I_map = {}
-ite = 100
-for t in range(ite):
+itelator = 100
+for t in range(itelator):
     for i in player:
         CFR("", i, t, 1, 1, I_map)
 
